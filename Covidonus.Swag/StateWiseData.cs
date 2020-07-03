1eea72f9ed40654f83081713cc9b997cec1edb81
Avnish Kumar <avnish@geninf.com>
2020-07-03 11:00:12 +0530

    Add images

---
+++ Covidonus.Swag/StateWiseData.cs
@@ -0,0 +1,10 @@
+﻿namespace Covidonus.Swag
+{
+    public partial class StateWiseData
+    {
+        public StateWiseData()
+        {
+            StateLogo = $"ms-appx:///Assets/States/{StateCode}.png";
+        }
+    }
+}
